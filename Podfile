108c4fef88a561373700682dc90fd9b6c869da15
Cielpy <beijiu572@gmail.com>
2015-11-24 18:49:06 +0800

    init

---
+++ Podfile
@@ -0,0 +1,26 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'V2EX-iOS' do
+
+pod 'Alamofire' , '~> 3.1.3'
+pod 'SQLite.swift', git: 'https://github.com/stephencelis/SQLite.swift.git'
+pod 'SwiftyJSON', '~> 2.3.1'
+pod 'Cartography', '~> 0.6.0'
+pod 'Kingfisher', '~> 1.8.2'
+pod 'Ji', '~> 1.2.0'
+pod 'AsyncSwift', '~> 1.4'
+pod 'ObjectMapper' , '~> 1.0.1'
+
+end
+
+target 'V2EX-iOSTests' do
+
+end
+
+target 'V2EX-iOSUITests' do
+
+end
+
---
+++ V2EX-iOSTests/V2EX_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  V2EX_iOSTests.swift
+//  V2EX-iOSTests
+//
+//  Created by ciel on 15/11/24.
+//  Copyright © 2015年 CL. All rights reserved.
+//
+
+import XCTest
+@testable import V2EX_iOS
+
+class V2EX_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
